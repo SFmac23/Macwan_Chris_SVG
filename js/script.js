07fe0d6a3fd211fda6e4ed98dcafa2326b7d7f7c
SFmac23 <chrismacwan2233@gmail.com>
2025-02-02 20:29:46 -0500

    code updated
    css, html and js code updated as needed. slight trials made.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-document.querySelectorAll(".icon").forEach(icon => {
-    icon.addEventListener("click", (event) => {
-        console.log(`Clicked icon ID: ${event.target.id}`);
-    });
-});
+var allIcons = document.querySelectorAll('div[class^=icon]');
+console.log("Found", allIcons.length, "div which class starts with icon");
+
+for (var i = 0; i < allIcons.length; i++) {
+    allIcons[i].addEventListener('click', function(icon) {
+    console.clear();
+    console.log("You clicked: ", icon.target.id);
+  });
+}
+
